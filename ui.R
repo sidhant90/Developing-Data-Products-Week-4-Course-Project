b6b0ad9a96956a6b51e6b4410e40f92f2544bc2d
Sidhant Gupta <sidhantgupta1990@gmail.com>
2021-03-20 09:36:35 +0530

    Add files via upload

--- ui.R
+++ ui.R
@@ -1,92 +1,51 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+library(plotly)
+library(colourpicker)
+library(ggplot2)
+library(gapminder)
+library(shinycustomloader)
+library(DT)
 
-library(shiny)
-
-shinyUI(
-    navbarPage("Shiny Application",
-               tabPanel("Analysis",
-                        fluidPage(
-                            titlePanel("The relationship between variables and miles per gallon (MPG)"),
-                            sidebarLayout(
-                                sidebarPanel(
-                                    selectInput("variable", "Variable:",
-                                                c("Number of cylinders" = "cyl",
-                                                  "Displacement (cu.in.)" = "disp",
-                                                  "Gross horsepower" = "hp",
-                                                  "Rear axle ratio" = "drat",
-                                                  "Weight (lb/1000)" = "wt",
-                                                  "1/4 mile time" = "qsec",
-                                                  "V/S" = "vs",
-                                                  "Transmission" = "am",
-                                                  "Number of forward gears" = "gear",
-                                                  "Number of carburetors" = "carb"
-                                                )),
-                                    
-                                    checkboxInput("outliers", "Show BoxPlot's outliers", FALSE)
-                                ),
-                                
-                                mainPanel(
-                                    h3(textOutput("caption")),
-                                    
-                                    tabsetPanel(type = "tabs", 
-                                                tabPanel("BoxPlot", plotOutput("mpgBoxPlot")),
-                                                tabPanel("Regression model", 
-                                                         plotOutput("mpgPlot"),
-                                                         verbatimTextOutput("fit")
-                                                )
-                                    )
-                                )
-                            )
-                        )
-               ),
-               tabPanel("About the Data Set",
+ui <- fluidPage(
+    
+    # App title ----
+    titlePanel("Gapminder Data Visualization using Shiny and Plotly"),
+    
+    # Sidebar layout with input and output definitions ----
+    sidebarLayout(
+        
+        # Sidebar panel for inputs ----
+        sidebarPanel(
+            
+            # Input: Select the random distribution type ----
+            textInput("title", "Title", "GDP vs life exp"),
+            numericInput("size", "Point size", 1, 1),
+            checkboxInput("fit", "Add line of best fit", FALSE),
+            colourInput("color", "Point color", value = "blue"),
+            
+            
+            selectInput("continent", "Continent",
+                        choices = c("All", levels(gapminder$continent))),
+            
+            sliderInput(inputId = "life", label = "Life expectancy",
+                        min = 0, max = 120,
+                        value = c(30, 50)),
+            downloadButton("download_data")
+            
+            
+        ),
+        
+        # Main panel for displaying outputs ----
+        mainPanel(
+            
+            # Output: Tabset w/ plot, summary, and table ----
+            tabsetPanel(type = "tabs",
                         
-                        h3("Regression Models Course Project (from Coursera)"),
-                        helpText("You work for Motor Trend, a magazine about the automobile industry Looking at a data set of a collection of cars, they are interested in exploring the relationship",
-                                 "between a set of variables and miles per gallon (MPG) (outcome). They are particularly interested in the following two questions: Is an automatic or manual transmission better for MPG. Quantify the MPG difference between automatic and manual transmissions"),
-                        h3("Important"),
-                        p("A data frame with 32 observations on 11 variables."),
+                        tabPanel("Plot", withLoader(plotlyOutput("plot")) ),
+                        tabPanel("Table", withLoader(DT::dataTableOutput("table")))
                         
-                        a("https://class.coursera.org/regmods-008")
-               ),
-               tabPanel("More Data Detail",
-                        h2("Motor Trend Car Road Tests"),
-                        hr(),
-                        h3("Description"),
-                        helpText("The data was extracted from the 1974 Motor Trend US magazine,",
-                                 " and comprises fuel consumption and 10 aspects of automobile design and performance",
-                                 " for 32 automobiles (1973-74 models)."),
-                        h3("Format"),
-                        p("A data frame with 32 observations on 11 variables."),
                         
-                        p("  [, 1]   mpg         Miles/(US) gallon"),
-                        p("  [, 2]	 cyl	 Number of cylinders"),
-                        p("  [, 3]	 disp	 Displacement (cu.in.)"),
-                        p("  [, 4]	 hp	 Gross horsepower"),
-                        p("  [, 5]	 drat	 Rear axle ratio"),
-                        p("  [, 6]	 wt	 Weight (lb/1000)"),
-                        p("  [, 7]	 qsec	 1/4 mile time"),
-                        p("  [, 8]	 vs	 V/S"),
-                        p("  [, 9]	 am	 Transmission (0 = automatic, 1 = manual)"),
-                        p("  [,10]	 gear	 Number of forward gears"),
-                        p("  [,11]	 carb	 Number of carburetors"),
-                        
-                        h3("Source"),
-                        
-                        p("Henderson and Velleman (1981), Building multiple regression models interactively. Biometrics, 37, 391-411.")
-               ),
-               tabPanel("Go back to my Github repository",
-                        a("https://github.com/manastiwari/Developing-Data-Products-Week-4-Course-Project"),
-                        hr(),
-                        h4("I hope you like the Shiny App"),
-                        h4("The name of the repository is Developing Data Products Week 4 Course Project")
-               )
+            )
+            
+        )
     )
 )
